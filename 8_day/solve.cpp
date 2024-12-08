bc891ae8009d5c65a69566f076ca7684bfba084b
DrunkenCloud <drunkencloud99@gmail.com>
2024-12-08 13:05:20 +0530

    some changes

--- 8_day/solve.cpp
+++ 8_day/solve.cpp
@@ -20,17 +20,8 @@ double distance(int x1, int y1, int x2, int y2) {
 }
 
 int main() {
-    cout << "Enter file (0 for input, 1 for trial): ";
-    int choice;
-    cin >> choice;
-    string fileName = (choice == 0) ? "input" : "trial";
-    ifstream file(fileName);
-    if (!file) {
-        cerr << "Error: Could not open file." << endl;
-        return 1;
-    }
-
-    int part1 = 0, part2 = 0;
+    ifstream file("input");
+    int ans = 0;
     vector<string> lines;
     string line;
     while (getline(file, line)) {
@@ -54,18 +45,11 @@ int main() {
                 }
             }
         }
-
-        if (points.size() == 0) continue;
-        cout << antenna << ": ";
-        for (auto& point : points) {
-            cout << '[' << point.first << ", " << point.second << "], ";
-        }
-        cout << " :\n";
         
-        if (points.size() == 1) continue;
+        if (points.size() <= 1) continue;
         for (auto& point : points) {
             if (!dp[point.first][point.second]) {
-                part2++;
+                ans++;
                 dp[point.first][point.second] = true;
             }
         }
@@ -86,17 +70,16 @@ int main() {
                 int anti_node_y = y1 - dy;
                 while (anti_node_x >= 0 && anti_node_x < m && anti_node_y >= 0 && anti_node_y < n) {
                     if (!dp[anti_node_x][anti_node_y]) {
-                        part2++;
+                        ans++;
                         dp[anti_node_x][anti_node_y] = true;
                     }
                     anti_node_x -= dx;
                     anti_node_y -= dy;
                 }
-                cout << "\n";
             }
         }
     }
 
-    cout << "Part2: " << part1 << endl;
+    cout << "Part2: " << ans << endl;
     return 0;
 }
